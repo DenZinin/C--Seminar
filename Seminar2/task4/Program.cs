bafb3bd4d48180569fa74e9153d9958af334676c
DenisZ <zinindenis2@gmail.com>
2023-01-21 23:43:09 +0300

    task4

---
+++ Seminar2/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 4 * : Напишите программу, которая выводит случайное трёхзначное число
+и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+*/
+
+int randnumber = new Random().Next(100, 1000);
+Console.WriteLine(randnumber);
+
+int cutnumber1 = randnumber / 100;
+int cutnumber2 = randnumber % 10;
+
+Console.Write(cutnumber1);
+Console.Write(cutnumber2);
