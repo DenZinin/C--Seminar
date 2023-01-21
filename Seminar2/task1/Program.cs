439383b70cc787dffe60dddd9053c34b2dd512a4
DenisZ <zinindenis2@gmail.com>
2023-01-21 19:45:49 +0300

    task1

---
+++ Seminar2/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает на вход трёхзначное число
+и на выходе показывает вторую цифру этого числа. 
+Не использовать строки для расчета.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine("Введите трехзначное число: ");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+
+int secondnumber = (usernumber/10)%10;
+
+Console.WriteLine($"Вторая цифра числа {usernumber} = {secondnumber}!");
