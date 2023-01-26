3650fd69e217ec5e2291f21f219caaf35bf23f5b
DenisZ <zinindenis2@gmail.com>
2023-01-26 23:11:46 +0300

    task2

---
+++ Seminar4/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Реализовать через функции.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    System.Console.WriteLine($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int usernumber = Prompt("Ведите число");
+
+int SumUserNumber(int usernumber)
+{
+    int sum = 0;
+    for (int i = usernumber; i > 0; sum += i % 10, i /= 10) ;
+    return sum;
+}
+
+int result = SumUserNumber(usernumber);
+
+System.Console.WriteLine($"Сумма цифр числа {usernumber} равна {result}!");
