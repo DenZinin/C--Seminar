48b57474f56fd86b3ed6709451f427e22883889e
DenisZ <zinindenis2@gmail.com>
2023-01-26 19:16:09 +0300

    task1

---
+++ Seminar4/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Реализовать через функции.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt(string message)
+{
+    Console.Write($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double usernumber1 = Prompt("Введите первое число");
+double usernumber2 = Prompt("Введите второе число");
+
+double DegreeNumber(double usernumber1, double usernumber2)
+{
+    double x = 1;
+    for (double i = 1; i <= usernumber2; i++);
+    {
+        double result = Math.Pow(usernumber1, usernumber2);
+        System.Console.WriteLine(result);
+    }
+    return x;
+}
+
+double x = DegreeNumber(usernumber1, usernumber2);
